af8bbf00348c960c37a95050d11daf8b58d31e77
PolinaKoriagina <koriagina.p@gmail.com>
2021-06-13 11:01:36 +0200

    added sout

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -9,5 +10,8 @@ public class TestBase {
     static void setup() {
         Configuration.startMaximized = true;
     }
-
+    @AfterAll
+    static void forPractice(){
+        System.out.println("Just for pull request");
+    }
 }
